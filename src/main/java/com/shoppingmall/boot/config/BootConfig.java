c6af43d118bd96b8dfb3e342c115b40a47d499c0
JiHyun, Jeong <39480072+dinb1242@users.noreply.github.com>
2022-06-07 22:55:45 +0900

    0607_기존 키 삭제 및 외부에서 환경 변수로 주입받도록 수정

--- src/main/java/com/shoppingmall/boot/config/BootConfig.java
+++ src/main/java/com/shoppingmall/boot/config/BootConfig.java
@@ -7,6 +7,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +18,12 @@ import javax.persistence.PersistenceContext;
 
 @Configuration
 public class BootConfig {
+
+    @Value("${aws.s3.access}")
+    private String access;
+
+    @Value("${aws.s3.secret}")
+    private String secret;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -35,7 +42,7 @@ public class BootConfig {
     public AmazonS3Client amazonS3() {
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion("ap-northeast-2")
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("AKIAWHXK4TWRFYKGIHHD", "nOLGgaf5nVdJfbCPiStVKzd7d92OE9y79rIEHmv1")))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(access, secret)))
                 .build();
     }
 
